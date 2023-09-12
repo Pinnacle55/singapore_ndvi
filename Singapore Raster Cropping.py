0293d1dbb2a67937b82150d902963b8ae72019a3
Jonathan Tan <jsw.tan1991@gmail.com>
2023-09-12 09:00:15 +0100

    Adjusted raster cropping to use command line arguments, added Earthpy tutorial

--- Singapore Raster Cropping.py
+++ Singapore Raster Cropping.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
+# In[6]:
 
 
 # Run this in an environment that has gdal installed
 import geopandas as gpd
 import numpy as np
 import os, sys
-
-
-# In[25]:
-
+from glob import glob
 
 study_area = gpd.read_file("singapore_boundary.geojson")
 
@@ -21,27 +18,31 @@ study_area = study_area.to_crs("EPSG:32648")
 xmin, ymin, xmax, ymax = study_area.total_bounds
 
 
-# In[22]:
-
-
-current_dir = os.getcwd()
-
-# list all files in 20230316
-filenames = os.listdir(os.path.join(current_dir, "20230316_IMG_DATA"))
-
-filenames = [os.path.join(current_dir, "20230316_IMG_DATA", file) for file in filenames]
-
-filenames
-
-
-# In[23]:
-
-
-# for file in filenames:
-#     print(os.path.join(current_dir, "20230316_IMG_DATA", file))
-
-
 # In[ ]:
+
+
+# provide target directory as first argument in command line call
+target_dir = sys.argv[1]
+
+band_files = glob(
+    os.path.join(
+        target_dir,
+        "LC08_L2SP*S*_B*.tif",
+    )
+)
+
+qa_files = glob(
+    os.path.join(
+        target_dir,
+        "*QA_PIXEL.tif",
+    )
+)
+
+# find all band files and qa files
+files = band_files + qa_files
+
+
+# In[12]:
 
 
 # Note that any existing stacked_cropped tif should be deleted before running
@@ -50,9 +51,9 @@ filenames
 # os.joined path names that have folders with spaces in them
 
 # You can work around this by manually adding open quotes to the command line call itself
-for file in filenames:
+for file in files:
     # -tr is a call that sets the resolution of the image
-    command4 = 'gdalwarp -t_srs {crs} -te {x_min} {y_min} {x_max} {y_max} -tr 10 10 -r bilinear "{src_file}" "{dst_file}" -co COMPRESS=DEFLATE'
+    command4 = 'gdalwarp -t_srs {crs} -te {x_min} {y_min} {x_max} {y_max} -tr 30 30 -r bilinear "{src_file}" "{dst_file}" -co COMPRESS=DEFLATE'
     os.system(command4.format(
         crs = f"EPSG:32648", 
         x_min = np.floor(xmin),
@@ -60,6 +61,12 @@ for file in filenames:
         x_max = np.ceil(xmax),
         y_max = np.ceil(ymax),
         src_file = f"{file}",
-        dst_file = f"{file.split('.')[0]}_UPSAMPLED_CROPPED.TIF"
+        dst_file = f"{file.split('.')[0]}_CROPPED.TIF"
     ))
+
+
+# In[ ]:
+
+
+
 
